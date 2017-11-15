36e5f31d2b77faaea7f62ac8878f385d18d8140d
Derek Strobel <dnstrobel000@gmail.com>
2017-11-15 12:03:26 -0700

    QPAgent finished

--- cartpole.py
+++ cartpole.py
@@ -4,17 +4,7 @@ import sys
 
 import gym
 from gym import wrappers
-
-from agents import QAgent
-
-
-class RandomAgent(object):
-    """The world's simplest agent!"""
-    def __init__(self, action_space):
-        self.action_space = action_space
-
-    def act(self, observation, reward, done):
-        return self.action_space.sample()
+from randomagent import RandomAgent
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
@@ -37,34 +27,29 @@ if __name__ == '__main__':
 
     env = gym.make(args.env_id)
 
-    env.seed(0)
-    agent = QAgent(env.action_space, env.observation_space, use_cuda=False)
-
-    episode_count = 100
-    reward = 0
-    done = False
-
     # You provide the directory to write to (can be an existing
     # directory, including one with existing data -- all monitor files
     # will be namespaced). You can also dump to a tempdir if you'd
     # like: tempfile.mkdtemp().
     outdir = '/tmp/random-agent-results'
     env = wrappers.Monitor(env, directory=outdir, force=True)
-    rewards = []
+    env.seed(0)
+    agent = RandomAgent(env.action_space)
+
+    episode_count = 1000
+    reward = 0
+    done = False
+
     for i in range(episode_count):
         ob = env.reset()
         while True:
-            action = agent.act(ob, reward, epsilon=1)
+            action = agent.act(ob, reward, done)
             ob, reward, done, _ = env.step(action)
-            rewards.append(reward)
             if done:
                 break
             # Note there's no env.render() here. But the environment still can open window and
             # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
             # Video is not recorded every episode, see capped_cubic_video_schedule for details.
+
     # Close the env and write monitor result info to disk
-    print('Average Reward over 100 episodes: {}'.format(sum(rewards)/len(rewards)))
     env.close()
-
-
-
--- cartpole/agents.py
+++ cartpole/agents.py
@@ -3,51 +3,91 @@ from torch.autograd import Variable
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-%matplotlib inline
+import random
 
-tensors = {False:torch.nn.FloatTensor, True:torch.cuda.FloatTensor}
+import pdb
+
+tensors = {False: torch.FloatTensor, True: torch.cuda.FloatTensor}
+
 
 class RandomAgent(object):
     """The world's simplest agent!"""
+
     def __init__(self, action_space):
         self.action_space = action_space
 
     def act(self, observation, reward, done):
         return self.action_space.sample()
 
-class QPAgent(object):
-    def __init__(self, action_space, initial_state,qLayers=3, pLayers=3, use_cuda=True, qlr=0.01, plr=0.01):
+
+class QAgent(object):
+    ''' 
+    Use Q network to approximate sum of future reinforcements
+    Action space must be Discrete
+    '''
+
+    def __init__(self, action_space, observation_space, qLayers=3, use_cuda=True, qlr=0.01, eld=0.99):
+        self.use_cuda = use_cuda and torch.cuda.is_available()
         self.tensorType = tensors[use_cuda]
+        self.eld = eld
+        self.epsilon = 1
         self.action_space = action_space
-        self.qlr, self.plr = qlr, plr
-        self.Q = Reltan(initial_state.shape[1]+1, h=10, o=1, layers=qLayers)
-        actionShape = 1
-        try:
-            actionShape = action_space.sample().shape[0]
-        except AttributeError as e:
-            pass
-        self.P = Reltan(initial_state.shape[1], h=10, o=actionShape, layers=pLayers)
+        self.qlr = qlr
+        self.Q = Reltan(observation_space.sample().shape[0] + 1, h=2, o=1, layers=qLayers)
+        self.prev = {'observation':None, 'reward':None, 'action':None}
 
-    def act(self, observation, reward, done):
-        return self.action_space.sample()
+    def act(self, observation, reward, maximize=True, epsilon=None, trainQIter=50):
+        '''
+        Assumes action space is spaces.Discrete
+        maximize=True assumes we want to choose the action that
+            maximizes the sum of future reinforcements
+        '''
+        if epsilon is None:
+            epsilon = self.epsilon
 
-    def oa2qin(observations, actions):
-        return self.tensorType(np.hstack(observations,actions.reshape(actions.shape[0],1)))
+        if random.random() > epsilon:
+            Qs = []
+            for action in range(self.action_space.n):
+                Qs.append(Q(oa2qin(observation, action)))
+            action = np.argmax(np.array(Qs))
+        else:
+            action = self.action_space.sample()
 
-    def trainQ(self, observations, actions, rewards):
-    	if observations.shape[0] is not rewards.shape[0]:
-    		raise ValueError('shape[0]s do not match; rows of observations and rewards must align')
-        qx = oa2qin(observations,actions)
-        qpred = Q(qx)
-        qprev = torch.cat((qpred[1:],self.tensorType([0]))) # add a zero at the end (will throw away this row after doing tensor math)
-        qy = qprev + self.qlr * (rewards -  qpred) # this is where you left off
-		#train Q
-		#train R
-		
+        if trainQIter and self.prev['observation'] is not None:
+            self.trainQ([self.prev['observation'],observation], [self.prev['action'],action], [self.prev['reward'],reward], trainQIter)
+
+        self.prev['observation'], self.prev['action'], self.prev['reward'] = observation, action, reward
+        return action
+
+    def oa2qin(self, observations, actions):
+        #return Variable(self.tensorType(np.hstack((observations, actions.reshape(actions.shape[0], 1)))))
+        return Variable(torch.cat((observations,actions), dim=1))
+
+    def trainQ(self, observations, actions, rewards, nIter):
+        # observations = np.array(observations)
+        # actions = np.array(actions)
+        # rewards = np.array(rewards)
+        observations = self.tensorType(observations)
+        actions = self.tensorType(actions)
+        rewards = self.tensorType(rewards)
+        if observations.shape[0] is not rewards.shape[0]:
+            raise ValueError(
+                'shape[0]s do not match; rows of observations and rewards must align')
+        qx = self.oa2qin(observations, actions)
+        qprev = self.Q(qx)
+        # add a zero at the end (will throw away this row after doing tensor
+        # math)
+        qcur = torch.cat((qprev[1:], self.tensorType([0])))
+        qy = qcur.data + self.qlr * (rewards - qprev.data)
+        qy = Variable(qy[:-1], requires_grad=False)  # remove that row at the end
+        # train Q
+        _train(self.Q, qx, qy, nIter, self.use_cuda)
+        #self.epsilon = self.eld * observations.shape[0] * self.epsilon
+
 
 def _train(model, x, y, nIter, use_cuda=True):
-    x = Variable(torch.FloatTensor(x))
-    y = Variable(torch.FloatTensor(y), requires_grad=False)
+    # x = Variable(torch.FloatTensor(x))
+    # y = Variable(torch.FloatTensor(y), requires_grad=False)
     if torch.cuda.is_available() and use_cuda:
         model = model.cuda()
         x = x.type(torch.cuda.FloatTensor)
@@ -71,18 +111,20 @@ def _train(model, x, y, nIter, use_cuda=True):
         optimizer.step()
     return lossTrace
 
+
 class Reltan(torch.nn.Module):
-    def __init__(self,n,h,o,layers=3):
-        super(Model, self).__init__()
+
+    def __init__(self, n, h, o, layers=3):
+        super(Reltan, self).__init__()
         self.mods = torch.nn.ModuleList([])
         self.mods.append(torch.nn.Linear(n, h))
         for layer in range(layers):
-	        self.mods.append(torch.nn.ReLU())
-	        self.mods.append(torch.nn.Tanh())
-        self.mods.append(torch.nn.Linear(h,o))
-        
+            self.mods.append(torch.nn.ReLU())
+            self.mods.append(torch.nn.Tanh())
+        self.mods.append(torch.nn.Linear(h, o))
+
     def forward(self, x):
-        #return self.fc(x) # it was just x there
+        # return self.fc(x) # it was just x there
         for module in self.mods:
             x = module(x)
         return x
