7978ea46a4f6beecd96c4deeabf72f72e4d094db
Derek Strobel <dnstrobel000@gmail.com>
2017-11-23 09:45:48 -0800

    Changed to SGD with momentum

--- cartpole/agents.py
+++ cartpole/agents.py
@@ -3,7 +3,10 @@ from torch.autograd import Variable
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+import torch.nn as nn
 import random
+import torch.nn.functional as F
 
 import pdb
 
@@ -22,25 +25,58 @@ class RandomAgent(object):
 def flat(tensor):
         return tensor.view(tensor.size()[0])
 
+class ExperienceReplay(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.o = []
+        self.r = []
+        self.a = []
+
+    def add(self, observations, rewards, actions):
+        self.o += observations
+        self.r += rewards
+        self.a += actions
+        if len(self.o) > self.capacity:
+            self.o.pop(0)
+            self.r.pop(0)
+            self.a.pop(0)
+
+    def sample(self, size):
+        o,r,a = [],[],[]
+        for i in range(size):
+            s = random.randint(0,len(self.o))
+            o += self.o[s]
+            r += self.r[s]
+            a += self.a[s]
+        return o,r,a
+
+    def isEmpty(self):
+        return len(self.o) > 0
+
 class QAgent(object):
     ''' 
     Use Q network to approximate sum of future reinforcements
     Action space must be Discrete
     '''
 
-    def __init__(self, action_space, observation_space, qLayers=5, qhidden=10, use_cuda=True, qlr=0.01, eld=0.99, gammaF=lambda x:0.3):
-        self.gamma = gammaF
+    def __init__(self, action_space, observation_space, qLayers=10, qhidden=30, use_cuda=True, qlr=0.1, eld=0.99, gammaF=lambda x:0.1):
+        self.replay = ExperienceReplay(10000)
+        self.gamma = 0.1
+        self.gammaF = gammaF
         self.use_cuda = (use_cuda and torch.cuda.is_available())
         self.tensorType = tensors[self.use_cuda]
         self.eld = eld
         self.epsilon = 1
         self.action_space = action_space
         self.qlr = qlr
-        self.Q = Reltan(observation_space.sample().shape[0] + 1, h=qhidden, o=1, layers=qLayers)
+        #self.Q = Reltan(observation_space.sample().shape[0] + 1, h=qhidden, o=1, layers=qLayers)
+        nin = observation_space.sample().shape[0] + 1
+        self.Q = nn.Sequential(nn.Linear(nin,qhidden), nn.ReLU6(), nn.ReLU6(), nn.ReLU6(), nn.Tanh(), nn.Tanh(), nn.Linear(qhidden,2), nn.Linear(2,1))
         if self.use_cuda:
             self.Q = self.Q.cuda()
         self.prev = {'observation':None, 'reward':None, 'action':None}
         self.lossTrace = np.array([])
+        self.epoch=0
 
     def _amaxQ(self, observation, ret='a'):
         Qs = []
@@ -73,7 +109,7 @@ class QAgent(object):
             self.trainQ([self.prev['observation'],observation], [self.prev['action'],action], [self.prev['reward'],reward], trainQIter)
 
         self.prev['observation'], self.prev['action'], self.prev['reward'] = observation, action, reward
-        self.epsilon *= self.eld
+        self.epsilon = max(self.eld * self.epsilon, 0.1)
         return action
 
     def oa2qin(self, observations, actions):
@@ -87,6 +123,7 @@ class QAgent(object):
         # observations = np.array(observations)
         # actions = np.array(actions)
         # rewards = np.array(rewards)
+        self.replay.add(observations,actions,rewards)
         observations = self.tensorType(observations)
         actions = self.tensorType(actions)
         rewards = self.tensorType(rewards)
@@ -96,19 +133,20 @@ class QAgent(object):
         qx = self.oa2qin(observations, actions)
         qs = self.Q(qx).data
         qs = flat(qs)
-        # add a zero at the end (will throw away this row after doing tensor math)
         # qcur = torch.cat((qprev[1:], self.tensorType([0])))
         # qcur = qcur.data.view(qcur.size()[0])
         #qy = qcur.data.view(qcur.size()[0]) + self.qlr * (rewards - qprev.data.view(qprev.size()[0])) # SARSA
         #pdb.set_trace()
         qopt = self.tensorType(np.max(np.array([[self.Q(self.oa2qin(o,a)).data[0] for a in range(self.action_space.n)] for o in observations]), axis=1))
         #pdb.set_trace()
-        qy = (1-self.qlr) * qs + self.qlr * (rewards + self.gamma(1) * qopt)  # QLearning
+        qy = (1-self.qlr) * qs + self.qlr * (rewards + self.gamma * qopt)  # QLearning
         #pdb.set_trace()
-        qy = Variable(qy, requires_grad=False)  # remove that row at the end
+        qy = Variable(qy, requires_grad=False)
         # train Q
         loss = _train(self.Q, qx, qy, nIter, self.use_cuda)
         self.lossTrace = np.append(self.lossTrace, loss)
+        self.epoch +=1
+        self.gamma = self.gammaF(self.gamma)
         #self.epsilon = self.eld * observations.shape[0] * self.epsilon
 
 
@@ -121,7 +159,8 @@ def _train(model, x, y, nIter, use_cuda=True):
     #     y = y.type(torch.cuda.FloatTensor)
 
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters())
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.1)
+    #optimizer = torch.optim.Adam(model.parameters(),lr=0.001)
     lossTrace = []
     for t in range(nIter):
         # Forward pass: Compute predicted y by passing x to the model
@@ -138,7 +177,6 @@ def _train(model, x, y, nIter, use_cuda=True):
         optimizer.step()
     return lossTrace
 
-
 class Reltan(torch.nn.Module):
 
     def __init__(self, n, h, o, layers=3):
@@ -146,12 +184,11 @@ class Reltan(torch.nn.Module):
         self.mods = torch.nn.ModuleList([])
         self.mods.append(torch.nn.Linear(n, h))
         for layer in range(layers):
-            self.mods.append(torch.nn.PReLU())
             self.mods.append(torch.nn.Tanh())
         self.mods.append(torch.nn.Linear(h, o))
 
     def forward(self, x):
         # return self.fc(x) # it was just x there
         for module in self.mods:
-            x = module(x)
+            module(x)
         return x
--- cartpole/cartpole.py
+++ cartpole/cartpole.py
@@ -9,6 +9,7 @@ from gym import wrappers
 from agents import QAgent
 
 
+
 class RandomAgent(object):
     """The world's simplest agent!"""
     def __init__(self, action_space):
@@ -39,9 +40,11 @@ if __name__ == '__main__':
     env = gym.make(args.env_id)
 
     env.seed(0)
-    agent = QAgent(env.action_space, env.observation_space, use_cuda=True, qlr=0.2, qLayers=10, qhidden=20)
+    #gammaF = lambda gp: 1 - 0.98*(1-gp)
+    gammaF = lambda gp: 0.99
+    agent = QAgent(env.action_space, env.observation_space, use_cuda=True, qlr=0.1, qLayers=2, qhidden=32, gammaF=gammaF,eld=0.99)
 
-    episode_count = 100
+    episode_count = 10000
     reward = 0
     done = False
 
@@ -50,22 +53,30 @@ if __name__ == '__main__':
     # will be namespaced). You can also dump to a tempdir if you'd
     # like: tempfile.mkdtemp().
     outdir = '/tmp/qlr-results'
-    env = wrappers.Monitor(env, directory=outdir, force=True)
+    env = wrappers.Monitor(env, directory=outdir, force=False)
     allRewards = []
+    allActions = []
     observations, rewards, actions = [],[],[]
+    oldo, oldr, olda = [],[],[]
     for i in range(episode_count):
         ob = env.reset()
         while True:
-            action = agent.act(ob, reward, trainQIter=None)
+            action = agent.act(ob, reward)
+            
             ob, reward, done, _ = env.step(action)
             observations.append(ob)
             rewards.append(reward)
             actions.append(action)
             if done:
                 break
-        agent.trainQ(observations,actions,rewards,10)
+        allActions += (actions)
         allRewards.append(sum(rewards))
+        agent.trainQ(observations,actions,rewards,64)
+        if not agent.replay.isEmpty():
+            agent.trainQ(*agent.replay.sample(min(i,1024)),48)
+        oldo, olda, oldr = observations, actions, rewards
         observations, rewards, actions = [],[],[]
+
 
             # Note there's no env.render() here. But the environment still can open window and
             # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
@@ -73,10 +84,13 @@ if __name__ == '__main__':
     # Close the env and write monitor result info to disk
     r = np.array(allRewards)
     print('Average Reward over 100 episodes: {}'.format(np.mean(r)))
-    fig,ax=plt.subplots(1,2)
+    fig,ax=plt.subplots(1,3)
     ax[0].plot(r)
+    ax[0].set_title('Reward Achieved')
     ax[1].plot(agent.lossTrace)
     ax[1].set_title('Loss Trace')
+    ax[2].plot(allActions,'.',alpha=0.2)
+    ax[2].set_title('Actions taken')
     fig.show()
     plt.show()
     env.close()
